7c7e748921bddb453de20d2c2b301f8a7dc5f034
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-04-10 09:30:39 +0530

    Day03

---
+++ java-basics-project/day03/src/main/java/com/herbalife/labs/lab05/Lab05.java
@@ -0,0 +1,73 @@
+package com.herbalife.labs.lab05;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Lab05 {
+    private static List<String> linesInCitiesCsvFile = null;
+    private static Map<String, List<City>> countryCitiesMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        linesInCitiesCsvFile = Files.readAllLines(Paths.get("./cities.csv"));
+        populateCountryCitiesMap();
+        //printAllTheCitiesGroupedByCountries();
+        printCityNameWithLargestAndSmallestPopulation();
+        printPopulationOf("China");
+    }
+
+    private static void printPopulationOf(String country) {
+        List<City> cities = countryCitiesMap.get(country);
+        long total = 0l;
+        for (City city : cities) {
+            total += city.getPopulation();
+        }
+        System.out.println(String.format("Population of %s is %s", country, total));
+    }
+
+    private static void printCityNameWithLargestAndSmallestPopulation() {
+        List<City> allCities = new ArrayList<>();
+        for (String country : countryCitiesMap.keySet()) {
+            allCities.addAll(countryCitiesMap.get(country));
+        }
+        City smallestCity = allCities.get(0);
+        City largestCity = allCities.get(0);
+
+        for (int i = 0; i < allCities.size(); i++) {
+            City currentCity = allCities.get(i);
+            if (currentCity.getPopulation() > largestCity.getPopulation()) {
+                largestCity = currentCity;
+            }
+            if (currentCity.getPopulation() < smallestCity.getPopulation()) {
+                smallestCity = currentCity;
+            }
+        }
+        System.out.println("Smallest city: " + smallestCity);
+        System.out.println("Largest city: " + largestCity);
+    }
+
+    private static void populateCountryCitiesMap() {
+        for (int i = 1; i < linesInCitiesCsvFile.size(); i++) {
+            String line = linesInCitiesCsvFile.get(i);
+            String[] items = line.split(",");
+            String country = items[1];
+            City city = new City(items[0], country, Long.parseLong(items[2]));
+            if (countryCitiesMap.containsKey(country)) {
+                List<City> cities = countryCitiesMap.get(country);
+                cities.add(city);
+            } else {
+                List<City> cities = new ArrayList<>();
+                cities.add(city);
+                countryCitiesMap.put(country, cities);
+            }
+        }
+    }
+
+    private static void printAllTheCitiesGroupedByCountries() {
+        System.out.println(countryCitiesMap);
+    }
+}
