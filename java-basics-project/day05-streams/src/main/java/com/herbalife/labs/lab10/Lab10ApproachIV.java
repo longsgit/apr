14905946746d165d9e8724a18c9cc8e27097fe3a
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-04-12 09:57:47 +0530

    Day05

---
+++ java-basics-project/day05-streams/src/main/java/com/herbalife/labs/lab10/Lab10ApproachIV.java
@@ -0,0 +1,41 @@
+package com.herbalife.labs.lab10;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+
+public class Lab10ApproachIV {
+    static List<Item> items = Arrays.asList(new Item("Nike", 20.45), new Item("Adidas", 31.45), new Item("Reebok", 29.25), new Item("Puma", 25.15), new Item("Fila", 15.15));
+
+    public static void main(String[] args) {
+
+        //items.stream().max(BinaryOperator.maxBy(Comparator.comparing(Item::getPrice)));
+
+        Item costliestShoe = items
+                .stream()
+                .collect(Collectors.maxBy(Comparator.comparing(item -> item.getPrice())))
+                .get();
+        System.out.println(costliestShoe);
+
+        Item cheapestShoe = items
+                .stream()
+                .collect(Collectors.minBy(Comparator.comparing(Item::getPrice)))
+                .get();
+        System.out.println(cheapestShoe);
+
+
+        concatNamesOfShoes();
+    }
+
+    private static void concatNamesOfShoes() {
+        String shoesInCsv = items
+                .stream()
+                .map(it -> it.getName())
+                .collect(Collectors.joining(", "));
+        System.out.println(shoesInCsv);
+    }
+
+
+}
