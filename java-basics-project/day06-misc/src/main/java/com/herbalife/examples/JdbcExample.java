0a6d6922dc801a6d1b58fcab00e6cfcd6a62afee
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-04-13 11:16:05 +0530

    Day06

--- java-basics-project/day06-misc/src/main/java/com/herbalife/examples/JdbcExample.java
+++ java-basics-project/day06-misc/src/main/java/com/herbalife/examples/JdbcExample.java
@@ -1,9 +1,6 @@
 package com.herbalife.examples;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JdbcExample {
     public static void main(String[] args) throws SQLException {
@@ -13,8 +10,35 @@ public class JdbcExample {
         //execute the statement
         //close connection
         //handle exceptions
-        createPerson("Ram", "Narain", 32);
-        createPerson("Mary", "Joe", 45);
+//        createPerson("Ram", "Narain", 32);
+//        createPerson("Mary", "Joe", 45);
+        loadAll();
+    }
+
+    private static void loadAll() throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
+            Statement statement = connection.createStatement();
+            String query = "select * from persons";
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int age = resultSet.getInt("age");
+                System.out.println("%s, %s, %s, %s".formatted(id, firstName, lastName, age));
+            }
+            resultSet.close();
+            statement.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 
     private static void createPerson(String firstName, String lastName, int age) throws SQLException {
