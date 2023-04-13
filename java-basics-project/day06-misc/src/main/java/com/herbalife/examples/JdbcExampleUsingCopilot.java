1367ff1f079fe1edf1038747664463ea1b53420c
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-04-13 11:43:09 +0530

    Day06

---
+++ java-basics-project/day06-misc/src/main/java/com/herbalife/examples/JdbcExampleUsingCopilot.java
@@ -0,0 +1,59 @@
+package com.herbalife.examples;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcExampleUsingCopilot {
+    public static void main(String[] args) throws SQLException {
+        createPerson("Sam", "Thompson", 34);
+        loadAllPersons();
+    }
+
+    private static void loadAllPersons() throws SQLException {
+        System.out.println("Loading all persons");
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.cj.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM persons");
+            while (resultSet.next()) {
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int age = resultSet.getInt("age");
+                int id = resultSet.getInt("id");
+                System.out.println("%s, %s, %s, %s".formatted(firstName, lastName, age, id));
+            }
+            resultSet.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+        }
+    }
+
+
+    private static void createPerson(String firstName, String lastName, int age) throws SQLException {
+        System.out.println("Creating person with first name: " + firstName + ", last name: " + lastName + ", age: " + age);
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.cj.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
+            connection.createStatement().execute("INSERT INTO persons (first_name, last_name, age) VALUES ('" + firstName + "', '" + lastName + "', " + age + ")");
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+
+        }
+
+    }
+
+}
