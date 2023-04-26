37f9a6fc849db81c9fdeb5cdc0caae9f63db596c
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-04-26 09:27:53 +0530

    Day14

---
+++ k8s-project/wednesday-api/src/main/java/com/herbalife/wednesdayapi/WednesdayApiApplication.java
@@ -0,0 +1,13 @@
+package com.herbalife.wednesdayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WednesdayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WednesdayApiApplication.class, args);
+    }
+
+}
---
+++ k8s-project/wednesday-api/src/test/java/com/herbalife/wednesdayapi/WednesdayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbalife.wednesdayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WednesdayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
