7da91c53e9e63b840553701c75105e6ec23cb785
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-04-26 07:41:03 +0530

    Day14

---
+++ spring-boot-kafka/producer-api/src/test/java/com/herbalife/producerapi/ProducerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbalife.producerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
